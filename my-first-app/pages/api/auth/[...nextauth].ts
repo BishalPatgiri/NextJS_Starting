e9399e7ab543a0867f4b346a75fde0d623d853db
BishalPatgiri <bishalpatgiri67@gmail.com>
2023-01-12 16:13:33 +0530

    second push

--- my-first-app/pages/api/auth/[...nextauth].ts
+++ my-first-app/pages/api/auth/[...nextauth].ts
@@ -2,14 +2,20 @@ import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import FacebookProvider from "next-auth/providers/facebook"
 import GithubProvider from "next-auth/providers/github"
+import EmailProvider from "next-auth/providers/email"
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter'
+    //@ts-ignore
+
+import clientPromise from "../../../database/connectDb"
+
+
 
 export const authOptions = {
+      //@ts-ignore
+
+  adapter: MongoDBAdapter(clientPromise),
   // Configure one or more authentication providers
   providers: [
-    FacebookProvider({
-        clientId: process.env.FACEBOOK_ID!,
-        clientSecret: process.env.FACEBOOK_SECRET!,
-      }),
       GithubProvider({
         clientId: process.env.GITHUB_ID!,
         clientSecret: process.env.GITHUB_SECRET!,
@@ -18,12 +24,25 @@ export const authOptions = {
         clientId: process.env.GOOGLE_ID!,
         clientSecret: process.env.GOOGLE_SECRET!,
       }),
+      EmailProvider({
+        server: {
+          host: process.env.EMAIL_SERVER_HOST,
+          port: process.env.EMAIL_SERVER_PORT,
+          auth: {
+            user: process.env.EMAIL_SERVER_USER,
+            pass: process.env.EMAIL_SERVER_PASSWORD,
+          },
+        },
+        from: process.env.EMAIL_FROM,
+      }),
   ],
     callbacks: {
     async session({ session, token, user }:any) {
       // const data=await prisma.onboarding.findMany({where:{entityShortName: 'Bishal'}})
       // console.log(data)
       // session.role=data[0].entityRepresentativeRole; 
+      console.log(session)
+      console.log(token)
       return session;
     }
   }
